21274b73de0e7abe4487fc041071502d96103bf3
Balbinoo <rodrigo080599@hotmail.com>
2024-11-25 15:06:40 +0100

    Configure port for CleverCloud

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { OeuvreModule } from './oeuvres.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OeuvreModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
