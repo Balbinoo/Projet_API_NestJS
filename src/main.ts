6e9762d14b870f2de627a8e40ac91e2679cd5bf0
Balbinoo <rodrigo080599@hotmail.com>
2024-11-27 13:48:41 +0100

    debugging

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { OeuvreModule } from './oeuvres.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OeuvreModule);
-  const port = process.env.PORT || 3000;
+
+  console.log('Starting the application after creating the module'); // Log application startup
+
+  const port = process.env.PORT || 8080;
+
+  console.log('The value of the port was defined,as ${port} '); // Log application startup
+
   await app.listen(port);
 }
 bootstrap();
