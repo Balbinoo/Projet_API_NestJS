dc427cf8d3907fc30bfe356755a0733a93dae011
Balbinoo <rodrigo080599@hotmail.com>
2024-11-30 10:25:55 +0100

    connect 8080

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { OeuvreModule } from './oeuvres.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OeuvreModule);
-
-  console.log('Starting the application after creating the module'); // Log application startup
-
-  const port = process.env.PORT || 8080;
-
-  console.log('The value of the port was defined,as ${port} '); // Log application startup
-
-  await app.listen(port);
+  await app.listen(8080);
 }
 bootstrap();
+
